4250cddd43e66d4abb6b6ce3b2cbc8c703404518
vsbecinstitution <104641987+vsbecinstitution@users.noreply.github.com>
2024-04-10 20:23:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import psycopg2
 # cursor = connection.cursor()
 
 
-
 db_config = {
     'dbname': 'verceldb',    # Replace with your PostgreSQL database name
     'user': 'default',      # Replace with your PostgreSQL username
